6c0d0523c2c348d2f07c3be886aafcb1227602d6
tanishk <tanucdi7@gmail.com>
2020-10-18 10:20:36 +0530

    Updates

---
+++ precache-manifest.aae34d7cc63b4f322701d67e723fcf64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d7809de7b550091d8dfab7b8eae159",
+    "url": "/csmind/index.html"
+  },
+  {
+    "revision": "f4dd8f72242df7756a75",
+    "url": "/csmind/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6ac0661b0699f624ce1e",
+    "url": "/csmind/static/css/main.d4ea41e9.chunk.css"
+  },
+  {
+    "revision": "f4dd8f72242df7756a75",
+    "url": "/csmind/static/js/2.6fcd1f98.chunk.js"
+  },
+  {
+    "revision": "eb745704df7294d386459c0622d6566d",
+    "url": "/csmind/static/js/2.6fcd1f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac0661b0699f624ce1e",
+    "url": "/csmind/static/js/main.7511c59f.chunk.js"
+  },
+  {
+    "revision": "ab320b1f94e40f4749dd",
+    "url": "/csmind/static/js/runtime-main.425e2c38.js"
+  }
+]);
