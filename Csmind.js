5c3a3cbf5928e04dfed701003b1dceee59a0657b
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-11-23 01:02:59 +0530

    WHAT IS CS MIND  PAGE

---
+++ Csmind.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import './threecs.css';
+const Csmind = () => {
+    return (
+        <div>
+            
+            <section id="call-to-action" class="wow fadeInUp">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center text-lg-left">
+            <h3 class="cta-title">CS MIND</h3> 
+            <b>You are known by what you create</b>
+            <p class="cta-text">CS MIND is the epitome of creation of new vision , ideas, , products, services  in various fields using state-of-the art - information technology , AI, IoT  through innovations in   products/services  to enrich the society in all respects . We work with talented youth in collaboration, harness their energies for self-creation   and creation of  the  systems of tomorrow.   We extend our years long experience of research  and  management acumen at CS MIND    for the benefit of all our collaborators and society .  We exist to realize the dreams of “ Make in India”   through  the  act of positive  disruption ,  and value oriented MIND set. </p>
+            <p class="cta-text">Today, the universities and industries have to work together engaging the youth ,trend setters and experts to create indigenous systems , products and services.  Establish ventures through research , design thinking, innovations  to achieve accelerated growth . CS MIND is a platform where new ideas take birth . We  mentor and  help shape future of the youth / professionals  at all levels through its large group of experts across the globe. The exponential technologies today are reshaping the education and the society as a whole . This is where we pitch in to make it happen.  </p>
+            <p class="cta-text">We invite the teens, the youth the universities and the industry to engage with us to create a rewarding  future for all of us. </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+    <section id="services" class="section-bg">
+      <div class="container">
+
+        <header class="section-header">
+          <h3>Key Offerings at CS MIND</h3>
+          <p>CS MIND provides you new vision, ideas, products, services in various fields.</p>
+        </header>
+
+        <div class="row">
+
+          <div class="col-md-6 col-lg-4 wow bounceInUp" data-wow-duration="1.4s">
+            <div class="box">
+              <h4 class="title"><a href="">Innovation at CS MIND </a></h4>
+              <p class="description">This is the spirit of CSMIND . We strive to bring research to life by design thinking , innovations , and incubation to cultivate startups and ventures exploring the technology of today , which could transform the ways of our present practices in the society , education , management, and industry/professional sectors. We work with universities, faculty and talented students and create systems for the future. </p>
+        <br/>      <a href="#" class="btn btn-outline-primary rounded-pill">Learn More</a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 wow bounceInUp" data-wow-duration="1.4s">
+            <div class="box">
+              <h4 class="title"><a href="">Aim at CS MIND</a></h4>
+              <p class="description">    Today the academics – the professional education and underlying systems are facing disruptive challenges . New methods/models are evolving in education, new courses are coming into existence and opening up unprecedented professional opportunities for the youth. One of the prominent opportunity is to create your own future through creativity , innovations and incubation.</p>
+        <br/>      <a href="#" class="btn btn-outline-primary rounded-pill">Learn More</a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 wow bounceInUp" data-wow-duration="1.4s">
+            <div class="box">
+              <h4 class="title"><a href="">3C at CS MIND</a></h4>
+              <p class="description">Counseling , Continuous improvement and Commit to self are the Career growth factors which can’t be ignored. Sailing on the competitive and varied opportunities canvas , with out proper mentoring may lead someone no where . Whether it is choosing programs , playing a big role , career growth , etc ., are difficult to decide . Whenever we put our signature, we actually commit something important.</p>
+        <br/>      <a href="#" class="btn btn-outline-primary rounded-pill">Learn More</a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+
+        </div>
+    )
+}
+
+export default Csmind
