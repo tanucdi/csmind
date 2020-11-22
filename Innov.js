7e65cd7d879b8a7d3e9f034bc284fd001cc76a79
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-11-22 11:00:21 +0530

    INNOVATION page....

---
+++ Innov.js
@@ -0,0 +1,133 @@
+import React from 'react'
+import './innov.css';
+
+const Innov = () => {
+    return (
+        <>
+        <div class="page-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 offset-lg-1 py-3 mt-lg-5 wow fadeInUp">
+        <h1>Innovations at CS MIND</h1>
+        <p class="mb-4">This is  the  spirit of  CSMIND  . We strive  to bring research to life by design thinking , innovations , and incubation to cultivate startups and  ventures  exploring the technology of today ,  which could transform the ways of our  present practices in the society , education , management, and  industry/professional sectors.   We work with universities, faculty and talented students and create systems for the future .</p>
+        <a href="#" class="btn btn-outline-primary rounded-pill">Fill The Form</a>
+      </div>
+      <div class="col-lg-5 py-3">
+        <div class="img-place mobile-preview shadow wow zoomIn">
+          <img src="media/pic07.jpg" alt="la" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+   <br/><br/>    <div class="position-realive bg-image" >
+<div class="page-section">
+  <div class="container">
+  <h1 center >The present ongoing ventures are :</h1>
+    <div class="row">
+        
+      <div class="col-lg-5 py-3">
+        <div class="img-place mobile-preview shadow wow zoomIn">
+          <img src="media/pic07.jpg" alt=""/>
+        </div>
+      </div>
+      <div class="col-lg-6 py-3 mt-lg-5">
+        <div class="iconic-list">
+          <div class="iconic-item wow fadeInUp">
+            <div class="iconic-md iconic-text bg-warning fg-white rounded-circle">
+              <span class="mai-cube"></span>
+            </div>
+            <div class="iconic-content">
+              <h5>Resilience Management.</h5>
+              <p class="fs-small">A smart city cant reach its goals unless it is also made residential to the threats  - which would include  threats from disasters , accidents ,and overall health of a city.</p>
+              <a href="#1" class="btn btn-outline-success rounded-pill">Read more</a>
+            </div>
+          </div>
+          <div class="iconic-item wow fadeInUp">
+            <div class="iconic-md iconic-text bg-info fg-white rounded-circle">
+              <span class="mai-shield"></span>
+            </div>
+            <div class="iconic-content">
+              <h5>TeamQ project</h5>
+              <p class="fs-small">Today  different companies have operations across the globe . A large number of  of professionals and experts are deployed on the projects who are best suited for a project. </p>
+              <a href="#2" class="btn btn-outline-success rounded-pill">Read more</a>
+            </div>         
+          </div>
+          <div class="iconic-item wow fadeInUp">
+            <div class="iconic-md iconic-text bg-warning fg-white rounded-circle">
+              <span class="mai-analytics"></span>
+            </div>
+            <div class="iconic-content">
+              <h5>DiaM</h5>
+              <p class="fs-small">DiaM is a diabetese management application. 
+Type II diabetese  has become a wide spread menace to the health of a common man.</p>
+              <a href="#3" class="btn btn-outline-success rounded-pill">Read more</a>
+            </div>
+          </div>
+          <div class="iconic-item wow fadeInUp">
+            <div class="iconic-md iconic-text bg-info fg-white rounded-circle">
+              <span class="mai-aperture"></span>
+            </div>
+            <div class="iconic-content">
+              <h5>Mechatroncs</h5>
+              <p class="fs-small">Dear Tanishq 
+Add all these writeups in our website in HTML format under service.</p>
+              <a href="#" class="btn btn-outline-success rounded-pill">Read more</a>
+            </div>         
+          </div>
+        </div>
+        </div>
+      </div> <br/>
+      <div class="row">
+          <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
+            <h3 id="1">Resilience Management</h3>
+            <b>A Disaster Management Project</b>
+             <ul class="font-Poppins">
+                 <br/>
+              <li><i class="icofont-check-circled"></i> A smart city cant reach its goals unless it is also made residential to the threats  - which would include  threats from disasters , accidents ,and overall health of a city. </li>
+              <li><i class="icofont-check-circled"></i> ResM is a framework  to address these challenges using exponential technologies like AI, IT , and GPS  deployed  upon a robust model based on efficient protocols of communication , coordination , collaboration and cooperation to bring in clarity in operations of actors involved to make a city relilient to handle eventualities. </li>
+              <li><i class="icofont-check-circled"></i> It is a cooperative model between the administration , experts and the society as a whole.   The design is based on  the research performed to explore use of technology for the benefit of the society to ensure safety ,quick and coordinated response , based on remote data  gathered across a city. The model is service orientd and can be deployed at district livel across the country and the globe. </li>
+          
+            </ul>
+          </div>
+        </div>
+<hr/>
+        <div class="row">
+          <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
+            <h3 id="2">TeamQ project</h3>
+            <b>A HR product.</b>
+             <ul class="font-Poppins">
+                 <br/>
+              <li><i class="icofont-check-circled"></i> Today  different companies have operations across the globe . A large number of  of professionals and experts are deployed on the projects who are best  suited for a project   . The projects are cyclic  and  new projects keep on coming , call for deploying the professionals on new projects as well as induct new members in the teams. </li>
+              <li><i class="icofont-check-circled"></i> Managing this function by the HR becomes a challanging job . Deployment of a best fit team on a project can ensure effective and efficient compltion of the projects This would  lead to customer and employee satisfaction  . Matching team compentencies with the project attributes is the key  factor and becomes very challenging in real-time environment.  </li>
+              <li><i class="icofont-check-circled"></i> TeamQ is an AI based service to acomplish a team building to process of a company, on realtime basis,  suit an upcoming project imbibing the companies values , strategies and competence . The TeamQ platform needs data of prospective professionls of a company and the project attributes  to determine the team. </li>
+            Further , as another function , individual professionals in the field can also use for their own profiling , improvement in their attributes through training to embark on new challanges.
+            </ul>
+          </div>
+        </div>
+<hr/>
+        <div class="row">
+          <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
+            <h3 id="3">DiaM</h3>
+            <b>DiaM is a diabetese management application. </b>
+             <ul class="font-Poppins">
+                 <br/>
+              <li><i class="icofont-check-circled"></i> Type II diabetese  has become a wide spread menace to the health of a common man . This is due to the changes in the  life style today and it is now called as lifestyle dieses . Ones this dieases is detected then its reversal is impossible and one has to learn to live with it and manage  ,which depends on multiple factors . As the age advances it starts affecting the health severly and may become threat yo the life. </li>
+              <li><i class="icofont-check-circled"></i>Therefore, proper medication ,continuous  monitoring , and self  discipline become  mandatory.  This is generally  difficult  to accomplish .
+DiaM is a mobile based AI application innovated to help a person to manage type II diabetese effectively  with  ease ,comfort  and confidence. The application not only does connect to self  to build the will power but  also connects to the doctor for monitoring and mentoring. The AI engine smartly categorizes one , regulates  life style  and help understand how one is handling this dieses ,which utilizes a number of human  parameters. </li>
+              
+            </ul>
+          </div>
+        </div>
+    </div>
+    
+  </div>
+  
+</div>   
+
+        </>
+    )
+}
+
+export default Innov
