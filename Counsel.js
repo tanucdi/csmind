5967f025de9e2e770cf0553a9a8d2122519b61ee
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-11-23 00:59:43 +0530

    COUNSELING PAGE

---
+++ Counsel.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './signup.css';
+
+const Counsel = () => {
+    return (
+        <div>
+            
+            <section id="intro" class="clearfix">
+    <div class="container d-flex h-100">
+      <div class="row justify-content-center align-self-center">
+        <div class="col-md-6 intro-info order-md-first order-last">
+          <h2> <span>Counseling | Mentoring | Guidance | Advises </span><br/></h2>
+          <div>
+           <p>The counseling/Mentoring /Guidance/Advises could be provided on calls, whatsup or email. If someone required personal meeting that could be held separately.</p>
+            <a href="#about" class="btn-get-started scrollto">Consult Now</a>
+          </div>
+        </div>
+  
+        <div class="col-md-6 intro-img order-md-last order-first">
+          <img src="media/coun.png" alt="" class="img-fluid"/>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+
+        </div>
+    )
+}
+
+export default Counsel
