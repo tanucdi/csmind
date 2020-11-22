33b2c24db81957e96e06e25a37149d2072bf3fed
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-11-22 10:55:41 +0530

    PROFILE PAGE .. IMAGE PATH IS SAME AS ON FRONT PAGE

---
+++ Profile.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import './profile.css';
+
+const Profile = () => {
+    return (<div>
+						<div class="wrapper style1">
+
+<div class="container">
+	<div class="row gtr-200">
+		<div class="col-12 col-12-mobile imp-mobile" id="content">
+			<article id="main">
+				<header>
+					<h2><a href="#">Mr. Pradip Chande  <svg class="bi bi-award-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+						<path d="M8 0l1.669.864 1.858.282.842 1.68 1.337 1.32L13.4 6l.306 1.854-1.337 1.32-.842 1.68-1.858.282L8 12l-1.669-.864-1.858-.282-.842-1.68-1.337-1.32L2.6 6l-.306-1.854 1.337-1.32.842-1.68L6.331.864 8 0z"/>
+						<path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+					  </svg>
+					  </a> </h2> 
+					<p>
+					|<strong> Chairman & Managing Director</strong> |
+					</p>
+				</header>
+				<a href="#" class="image featured"><img src="media/download.jpg" alt="" /></a>
+				<p class="para"> I have more than 40 years of proven experience in institute building :  teaching, research,  consulting, management, administration and have  had occupied many key positions as Director in past 20 years. Recently superannuated from the Director’s position at the School of Business Management, MNIMS, Indore. Presently lead a large team of young  innovators  to make their future and  incubating products for social use.
+				<hr/>
+				</p>
+				<section>
+					<header>
+						<h3>Administration and Leadership </h3>
+					</header>
+					<p class="para">
+					I   headed Shri. GS Institute of Technology & Science, Indore,  as its Director . This  is one of the  premier  autonomous   institutions in the country . I had led its grand Golden Jubilee Celebrations in 2002; established a Technology Incubation Center with   Govt. grants -  the then  President of India , Dr APJ Abdul Kalam had laid  its  foundation stone; I played a leading role  to pursue  the Deemed- to- be- University project  of the institute,  had obtained notable  grants from  the  World Bank Project IMPECT and  TEQIP, and had   also  worked as a counselor & auditor for this project in the state.	As the  director of the institute I  had enhanced its research outcome , established infrastructure  and  enhanced academic culture. .
+
+I was  the  Corporate Head at Mahindra British Telecom Limited, Pune, where I  was responsible for the development of Mahindra Institute of Software Engineering & Management in collaboration with the  CMU, USA for new  PG Programs in Information Security, Software Engineering;  and  Management Development Programs for Industries. 
+
+I had pursued  a proposal to establish MISEM ( Mahindra Institute of Software Engineering and Management) as  a De-Novo   University  with the said programs  . I was responsible to  build a strong team and  manpower  for  Mahindra British Telecom Ltd.,  under the leadership of Mr. Anand  Mahindra.
+
+I  was the Founding  Director & Professor of IT at Maulana Azad National Institute of Technology (A Deemed University), Bhopal    , and was also Director-in-charge of MP Science & Technology Entrepreneurship Park, Bhopal. During this tenure I had successfully transformed the REC into a deemed-to-be university. 
+
+This included academic reforms ,cultural reforms, promoted quality research,  enhanced industry partnership and  consulting . My tenure as its Director was highly appreciated by the successive chairmen.
+
+Following this tenure I  took the opportunity to join IIM Indore  as its  full time    Professor  of Information Systems,  and was also the chair  for Innovation  & Entrepreneurship; Chair - Management Development program. Developed leading programs like  Knowledge Management ,  coauthored books on information systems , published by Oxford Press  ,delivered MDPs to industries on KM.	
+
+I have also received recognition from the state Govt.who  had invited  me  as  one of  the Directors on the board of MP Electronics Corporation, Bhopal. I was   Involved in a number of state level projects of SEZs in IT and Electronics, IT infrastructure, industry-academia partnership etc.
+
+Subsequently , I was invited by the TRUBA group of institutes ( now SAGE University), to lead the group as its Group Director . The role  had included strategy formation , establishing foreign collaborations, mentoring/faculty development, quality enhancement, product development, research,  and accreditation  . A pioneering PG program on  Business Technologies   was developed by me which was the outcome of   the national conference on this topic , as its chair.
+
+					</p> 
+				</section>
+				<section>
+					<header>
+						<h3>Academic and  Professional Activities</h3>
+					</header>
+					<p class="para">
+						
+					At SGSITS Indore, as  a Professor  and Head of Computer Engineering , I was  responsible for the  World Bank IMPACT project  as its coordinator,  acted as  the General Chair of the  International Conference on Automation - ICAUTO-95, sponsored by SICE ( the Society for Control and Instrument Engineers  ) Japan. As a Professor I have  edited/reviewed many journals/ conferences proceedings in this  area of  research .  I also   conducted workshops in Japan and Malaysia on Intelligent Robotics and Smart Vehicular Systems while  I was   visiting Professor at Kumamoto University, Japan. I have delivered a number of invited talks in  the areas of Management and Automation on various platforms.
+
+During this tenure as Professor and Chair of Computer science at SGSITS, Indore    I   had  established a center for AI & Robotics  with MHRD grants  and  a PG program in Automation.  I have worked as  the  member of AICTE - PG Board,  and then  member of many other Boards, Senates and RDCs of different Universities. I am an  invited member of Society of Instruments and Control Engineering Japan  (SICE)  and was  also the  member of e-governance, e-education, tele-medicine Committees of MP Electronics Corporation and Rajiv Gandhi Technical University.   At various stages of my  profession  I was instrumental in collaborating  and consulting with BHEL, Bhopal, Pestronics, Indore, State Bank of Indore, Gajra Gears, SOPA, Web-Dunia and M & M.     
+
+
+					</p>
+				</section>
+				<section>
+					<header>
+						<h3>Research & current interests</h3>
+					</header>
+					<p class="para">
+					I have  pursued research and professional activities at national and  international levels in the areas like Fault Tolerant Systems, Computer Networks, Multi-processors, Real Time Knowledge Bases Systems, Intelligent Automation, Medical Informatics, Human Centric System, Smart Vehicular Systems, etc.  I have  guided 12 PhDs and have  published around 100 research papers in international / national journals / conferences.     Presently I am guiding many researchers and innovators. 
+
+My current research interests include  : knowledge Management, Human Centric Systems, Man-machine Collaboration, Systems for Physically/mentally Challenged, IT Project Management, Decision Support Systems, Educational Management & Innovation, Intelligent Agents, Group Support System, Behavioral Engineering, Distance Education, and e-governance, Information Architectures and evolutionary organizations. 
+		
+				</p>
+				</section>
+				<section>
+					<header>
+						<h3>Awards/Honors</h3>
+					</header>
+					<p class="para">
+						
+					I am a recipient of various national & international honours & awards, viz, International Scholarship Award by Society of Instrument & Control Engineering, Japan, FANUC Japan scholarship, Anna University National Award for Best Academics 1996, Pt. Lajja Shankar Jha Award for Outstanding Contribution to Technology 1994, Radhakrishnan Award 1992 for research contribution in multiprocessor performance, etc. I am a senior  member of various Institutional and professional bodies including IEEE. 
+					I   invited to deliver the MS Ramanujam Memorial Lecture on the occasion of Seventh Institution of Engineers’ National Convention of Computer Engineers on the topic of Information Security and, to deliver  Institution of Engineers  theme lecture on “Role of Science in technology and its impending challenges”.
+
+I am a graduate of Electrical Engineering  -1974, Postgraduate in Applied  Electronics -1978, and PhD  in Engineering  1986 from Indore University( now Devi Ahilya University), Indore.
+<br/>
+<strong><br/>-Prof. P K Chande</strong></p><hr />
+				</section>
+			</article>
+		</div>
+	</div>
+	
+</div>
+</div> 
+		</div>
+    )
+}
+
+export default Profile
