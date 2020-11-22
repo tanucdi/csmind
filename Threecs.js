41da5e98c420a804f5a229f303813a2699a8daff
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-11-22 22:08:48 +0530

    3c at cs mind.... sign up will be up soon

---
+++ Threecs.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './threecs.css';
+
+const Threecs = () => {
+    return (
+        <div>
+            
+            <section id="call-to-action" class="wow fadeInUp">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center text-lg-left">
+            <h3 class="cta-title">3C at CS MIND</h3>
+            <p class="cta-text">Counseling  , Continuous improvement and  Commit to self are  the  Career growth factors which can’t be ignored . At CS  MIND we strive to enrich these for the career growth .  Sailing on  the  competitive and varied opportunities canvas ,  with out proper mentoring  may lead someone no where  . Whether it is choosing  programs  , playing a big role ,  career  growth  , etc ., are  difficult to decide .  Other interesting aspect is the commitment ;  self  commitment and   whenever we put our signature,  we actually commit something important  . Signature does  represent a personality and lead to prospects . which is generally ignored . Here we facilitate , not only counseling but how to sign up the commitment. </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+    <section id="services" class="section-bg">
+      <div class="container">
+
+        <header class="section-header">
+          <h3>Services</h3>
+          <p>CS MIND provides you counseling and also how to sign up the commitment.</p>
+        </header>
+
+        <div class="row">
+
+          <div class="col-md-6 col-lg-6 wow bounceInUp" data-wow-duration="1.4s">
+            <div class="box">
+              <h4 class="title"><a href="">Counseling / Mentoring </a></h4>
+              <p class="description">The counseling/Mentoring /Guidance/Advises could be provided on calls , whatsup or email. If someone required personal meeting that could be held separately. </p>
+        <br/>      <a href="#" class="btn btn-outline-primary rounded-pill">Fill The Form</a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-6 wow bounceInUp" data-wow-duration="1.4s">
+            <div class="box">
+              <h4 class="title"><a href="">Sign Up the Future</a></h4>
+              <p class="description">    Today, the youth and teens have enormous career opportunities – so are the challenges to fulfill their dreams. Every career needs talent, hard work and opportunities to shine bright.</p>
+        <br/>      <a href="#" class="btn btn-outline-primary rounded-pill">Fill The Form</a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+
+        </div>
+    )
+}
+
+export default Threecs
