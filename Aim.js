f0423b0850a93efbc01fb0e85d171022f33b5b30
tanishk chaturvedi <44547359+tanucdi@users.noreply.github.com>
2020-11-22 10:57:13 +0530

    AIM PAGE.. PLEASE MOVE IMG TO MEDIA

---
+++ Aim.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import './aim.css';
+import './innov.css';
+const Aim = () => {
+    return (
+        <div>
+
+<div class="hero-caption">
+    <div class="container fg-white h-100">
+      <div class="row justify-content-center align-items-center text-center h-100">
+        <div class="col-lg-6">
+          <h2 class="mb-4 fw-medium">AIM at CS MIND</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<div class="page-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-5 py-3">
+        <div class="img-place mobile-preview ">
+          <img src="media/aims.png" alt="la" />
+        </div>
+      </div>
+      <div class="col-lg-6 offset-lg-1 py-3 mt-lg-5 wow fadeInUp">
+        <p class="mb-4">Today the academics – the   professional education and   underlying systems  are facing disruptive challenges .  New methods/models are evolving in education, new  courses are coming into existence and opening up unprecedented professional opportunities for the youth.  One of the prominent opportunity is to create your own future through creativity , innovations and incubation. CS MIND is available to meet such challenges along with the universities , and industry professionals  in a win-win environment . </p>
+    <a href="#" class="btn btn-outline-warning rounded-pill">Fill The Form</a>
+  </div>
+   </div>
+  </div>
+</div>
+<br/>
+
+<div class="page-section">
+  <div class="container">
+  <h3 class="mb-4 haha"><strong>AIM at CS MIND Includes:</strong></h3>
+    <div class="row">
+    
+      <div class="col-lg-4 py-3">
+          
+        <div class="iconic-list">
+      
+            <div class="iconic-content">
+              <h5>Preparing for the new education paradigm </h5>
+               </div>
+               <br/>
+          
+            <div class="iconic-content">
+              <h5>Thrusting the research to  the ventures</h5>
+               </div>
+            
+               <br/>
+          
+            <div class="iconic-content">
+              <h5>Designing thinking to  Products  realization</h5>
+              </div>
+          </div>
+       
+      </div>
+      <div class="col-lg-4 py-3 wow zoomIn">
+        <div class="img-place mobile-preview shadow">
+          <img src="media/aim.gif" alt=""/>
+        </div>
+      </div>
+      <div class="col-lg-4 py-3">
+        <div class="iconic-list">
+         
+            <div class="iconic-content">
+              <h5>Faculty development and joint ventures </h5>
+              </div>
+              <br/>
+          
+            
+            <div class="iconic-content">
+              <h5>Quality, competitive skills , strategic planning </h5>
+             </div>
+          <br/>
+         
+           
+            <div class="iconic-content">
+              <h5>Building the  institutes of excellence </h5>
+               </div>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+        </div>
+    )
+}
+
+export default Aim
